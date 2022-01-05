c7363db6ac7434d8b9d11e0f412ab81b1d1e79d4
Moisés R. Martins <moisesmartins.box@gmail.com>
2022-01-05 16:00:38 -0300

    Calculadora: Aprimoramento do Javascript

--- calculadora/scripts.js
+++ calculadora/scripts.js
@@ -1,3 +1,23 @@
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+        this.previousOperandTextElement= previousOperandTextElement
+        this.currentOperandTextElement=currentOperandTextElement
+        this.clear()
+    }
+
+    clear() {}
+
+    delete() {}
+
+    appendNumber(number) {}
+
+    chooseOperations(operation) {}
+
+    compute() {}
+
+    updateDisplay() {}
+}
+
 const numberButtons= document.querySelectorAll('[data-number]')
 const operationButtons= document.querySelectorAll('[data-operation]')
 const equalsButtons= document.querySelector('[data-equals]')
@@ -7,3 +27,10 @@ const previousOperandTextElementButtons= document.querySelector('[data-previous-
 const currentOperandTextElementButtons= document.querySelector('[data-current-operand]')
 
 const Calculator= new Calculator(previousOperandTextElement, currentOperandTextElement)
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.appendNumber(button.innerText)
+        calculator.updateDisplay()
+    })
+})
